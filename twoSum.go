43101ea3ca15dab0651693a85eddd583a799f6d2
agrimakp <qvistn111@gmail.com>
2022-08-28 14:47:35 +1000

    add two numbers in an array (  brute force algorithm)

---
+++ twoSum.go
@@ -0,0 +1,14 @@
+package main
+
+// brute force results ( algorithm has O(n^2) time complexity
+// Runtime: 30 ms, Memory Usage: 3.5 MB
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
