3f5ec2257f6d4f3fb8382effb02f6ae9ed43f0dc
agrimakp <qvistn111@gmail.com>
2022-10-28 11:30:45 +1100

    basic

---
+++ helloWorld.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	name1 := "Shidil"
+	name2 := "Shidil Eringa"
+	var name string
+	//1
+	fmt.Println("Hello World")
+	//2
+	fmt.Print("1: Hello ", name1)
+	fmt.Println("")
+
+	fmt.Printf("2: Hello %s", name2)
+	fmt.Println("")
+
+	// 3
+	fmt.Println("Your Name ?")
+	fmt.Scan(&name)
+	fmt.Printf("Name: %s", name)
+	fmt.Println("")
+
+	//4
+
+}
