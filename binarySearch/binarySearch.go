01194f48fb0b58aa897070c49b3c5b4e37d7d4ef
agrimakp <qvistn111@gmail.com>
2023-03-25 10:05:32 +1100

    added constraints

--- binarySearch/binarySearch.go
+++ binarySearch/binarySearch.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
 // You must write an algorithm with O(log n) runtime complexity.
-
+// constraints : 1 <= nums.length <= 104
 func search(nums []int, target int) int {
 	low := 0
 	high := len(nums) - 1
@@ -30,6 +30,7 @@ func search(nums []int, target int) int {
 func main() {
 	nums := []int{-1, 0, 3, 5, 9, 12}
 	target := 9
+	// target := 2
 
 	result := search(nums, target)
 
