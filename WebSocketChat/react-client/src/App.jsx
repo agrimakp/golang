686f373929e020773ce0e58b0efeb8eac620e541
agrimakp <qvistn111@gmail.com>
2023-04-08 21:23:59 +1000

    display  chat list and sidebar

--- WebSocketChat/react-client/src/App.jsx
+++ WebSocketChat/react-client/src/App.jsx
@@ -3,20 +3,18 @@ import logo from './logo.svg'
 import './App.css'
 
 let socket = new WebSocket("ws://localhost:4000/ws")
-const list = [1]
-const ChatList = (props) => {
-  return list.map(item => {
-    return (
-      <div key={item} className='chat-item'>
-        <div className='Avatar'>pic</div>
-        <div className='profile'>
-          <h3>{props.name}</h3>
-          <p>{props.message}</p>
-        </div>
-        <div className='time'>10:00</div>
+
+const ChatItem = (props) => {
+  return (
+    <div className={`chat-item ${props.isActive ? 'active' : ''}`}>
+      <div className='avatar'></div>
+      <div className='profile'>
+        <h3>{props.name}</h3>
+        <p>{props.message}</p>
       </div>
-    )
-  })
+      <div className='time'>{props.time}</div>
+    </div>
+  )
 }
 
 function App() {
@@ -45,7 +43,50 @@ function App() {
 
   return (
     <div className="App">
-       
+      <div className='container'>
+
+        {/* // sidebar */}
+        <div className='sidebar'>
+          <div className='avatar'>
+            <h3><b>A</b></h3>
+          </div>
+
+          <img src='./icon.png' className='icon' />
+          <img src='./icon.png' className='icon' />
+          <img src='./icon.png' className='icon' />
+
+        </div>
+
+        {/* // messages list */}
+        <div className='chatList'>
+          <div className='header'>
+            <h3>Messages</h3>
+            <span className='count'>12</span>
+            <div className='newChat'>+</div>
+          </div>
+          <input type='search' name='search' className='search' placeholder='Search messages' />
+
+
+          <ChatItem isActive name='chinchu' message='1Lorem ipsum dolor sit amet' time='10.00' />
+          <ChatItem name='manchu' message='2Lorem ipsum dolor sit amet' time='10.00' />
+
+        </div>
+
+
+        {/* chat */}
+        <div className='chat'>ddfsdfds</div>
+
+
+
+
+
+
+
+
+
+
+        <div className='chatDirectory'>ddfsdfds</div>
+      </div>
     </div>
   )
 }
@@ -53,12 +94,12 @@ function App() {
 export default App
 
 
-   {/* <ul>
+{/* <ul>
         {
           messages.map((m, i) => (
             <li key={i}>{m}</li>
           ))
         }
       </ul> */}
- {/* <input type="text" name="message" onChange={typeMessage} />
+{/* <input type="text" name="message" onChange={typeMessage} />
         <input type="submit" value="Send" onClick={sendMessage}></input> */}
